e210cfa855ecea7fba8156d87e247a413e1a9e4f
projetoeai <projeto.eai19@gmail.com>
2019-09-19 21:53:39 -0300

    modificações saulo

---
+++ SchoolControl/SchoolControl.Web/Areas/StudentArea/StudentAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SchoolControl.Web.Areas.StudentArea
+{
+    public class StudentAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "StudentArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "StudentArea_default",
+                "StudentArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SchoolControl/SchoolControl.Web/Areas/TeacherArea/TeacherAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SchoolControl.Web.Areas.TeacherArea
+{
+    public class TeacherAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TeacherArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TeacherArea_default",
+                "TeacherArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
